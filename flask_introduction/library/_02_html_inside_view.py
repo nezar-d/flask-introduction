351560059767a678b123e060a6d67bf92145af13
nezar-d <nezardimitri@hotmail.com>
2018-01-15 14:39:04 -0500

    added dynamic list

--- flask_introduction/library/_02_html_inside_view.py
+++ flask_introduction/library/_02_html_inside_view.py
@@ -18,13 +18,21 @@ def hello_world():
     html = """
         <html>
             <h1>Welcome to our Library!</h1>
-            <ul>
+            <!--<ul>
                 <li>Alan Poe</li>
                 <li>Jorge L. Borges</li>
                 <li>Mark Twain</li>
-            <ul>
+            <ul>-->
+            {authors_ul}
         </html>
     """
     authors = ["Alan Poe", "Jorge L. Borges", "Mark Twain"]
     # build an <ul> with authors
-    return html
+    
+    authors_list = "<ul>"
+    authors_list += "\n".join([
+        "<li>{author}</li>".format(author=author) for author in authors])
+    authors_list += "</ul>"
+    print(authors_list)
+    
+    return html.format(authors_ul=authors_list)
